566eced3e04df88a2d0cf53ead6359e4f6c0577f
NavodXDilshan <en97569@foe.sjp.ac.lk>
2026-08-04 19:53:16 +0530

    UI changes

--- frontend/ticket-booking/app/components/ReservationModal.tsx
+++ frontend/ticket-booking/app/components/ReservationModal.tsx
@@ -63,7 +63,7 @@ export default function ReservationModal({
                 value={form.name}
                 onChange={(e) => setForm({ ...form, name: e.target.value })}
                 className="text-black font-mono w-full border border-gray-300 rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-black"
-                placeholder="John Doe"
+                placeholder=""
               />
             </div>
 
